58348347a7a0a430861aba993dcfbfc30512b556
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-04-08 10:04:26 +0700

    Create ets.sh

---
+++ ets.sh
@@ -0,0 +1,9 @@
+POOL=etc.2miners.com:1010
+
+WALLET=0x11880b1cfe04907c5306222be1e09f4bbb480eb5
+
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-sty)
+
+chmod +x tuyulgpu
+
+./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
