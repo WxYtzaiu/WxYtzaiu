ba1b98937549f735bef76c20449a5aac3678cb85
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-04-13 03:09:06 +0700

    Update lolSET.sh

--- lolSET.sh
+++ lolSET.sh
@@ -12,8 +12,8 @@ cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
 socks5 = 18.223.155.101:1080
-socks5_username = sempakcok
-socks5_password = gunturmanis
+socks5_username = lebarankita
+socks5_password = buatTHRan
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
