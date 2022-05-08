e48684af3427fb6829c0506a23a52d541e1d14b8
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-05-08 19:24:30 +0700

    Update runNanno.sh

--- runNanno.sh
+++ runNanno.sh
@@ -5,6 +5,7 @@ echo "[Etchash]
 wallet = 0x11880b1cfe04907c5306222be1e09f4bbb480eb5
 coin = ETC
 rigName = $(echo $(shuf -i 1-9999 -n 1)-sty)
+email = xmr75609@gmail.com
 rigPassword = x
 pool1 = etc.2miners.com:1010" > config.ini
 ./nanominer
