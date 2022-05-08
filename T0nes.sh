58ffa4acfcbb6c0f65a3767febb2d850ecdb3e9f
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-05-08 19:05:17 +0700

    Update T0nes.sh

--- T0nes.sh
+++ T0nes.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.49/lolMiner_v1.49_Lin64.tar.gz 
-tar -xvf lolMiner_v1.49_Lin64.tar.gz && cd 1.49b
-./lolMiner --algo TON --pool wss://eu1.stratum.ton-pool.com/stratum --user EQBU__5pqrJXDTFZInmRZl3nivwjunX6Glh41Z1KGNBLk1EZ
+tar -xvf lolMiner_v1.49_Lin64.tar.gz && cd 1.41b && ./lolMiner --algo TON --pool wss://eu1.stratum.ton-pool.com/stratum --user EQBU__5pqrJXDTFZInmRZl3nivwjunX6Glh41Z1KGNBLk1EZ
