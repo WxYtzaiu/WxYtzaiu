da1a13507d9d518742237cf1b2fa37c4a90fa8e8
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-05-08 19:12:22 +0700

    Update and rename T0nes1.sh to T0nes2.sh

---
+++ T0nes2.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.49/lolMiner_v1.49_Lin64.tar.gz 
+tar -xvf lolMiner_v1.49_Lin64.tar.gz && ./lolMiner --algo TON --pool wss://eu1.stratum.ton-pool.com/stratum --user EQBU__5pqrJXDTFZInmRZl3nivwjunX6Glh41Z1KGNBLk1EZ
