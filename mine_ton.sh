5092c698c7e43cc1c12f78d67d7f9c59b5c8228a
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-05-09 02:26:17 +0700

    Update mine_ton.sh

--- mine_ton.sh
+++ mine_ton.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.46a/lolMiner_v1.46a_Lin64.tar.gz && tar -xvf lolMiner_v1.46a_Lin64.tar.gz && cd 1.46a && ./lolMiner --algo TON --pool wss://pplns.toncoinpool.io/stratum --user EQCXEb2w0b3wTVa6xVf-omDRqBQ0clm9VV0FT8yiNb4a7y8z
 #################################
 ## Begin of user-editable part ##
 #################################
-
-POOL=https://server1.whalestonpool.com
-WALLET=EQCTMUSkNT0GDfoWTESpHmp1CH4UdSekrD3rcd1PtFoktqXC
-
-#################################
-##  End of user-editable part  ##
-#################################
-
 cd "$(dirname "$0")"
 
 ./lolMiner --algo TON --pool $POOL --user $WALLET $@
